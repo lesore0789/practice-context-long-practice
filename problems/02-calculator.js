147227ed1d4b797c12e204f703292252f7158e8f
Leslie <lesore0789@gmail.com>
2023-01-31 19:43:15 -0800

    Finished first three problems

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,5 +1,23 @@
-// Your code here
+class Calculator{
+	constructor(total = 0){
+		this.total = total;
+	}
 
+	add = num => this.total += num;
+
+	subtract = num => this.total -= num;
+
+	divide = num => this.total /= num;
+
+	multiply = num => this.total *= num;
+}
+
+let calculator = new Calculator();
+console.log(calculator.add(50));      // => 50
+console.log(calculator.subtract(35)); // => 15
+console.log(calculator.multiply(10)); // => 150
+console.log(calculator.divide(5));    // => 30
+console.log(calculator.total)         // => 30
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
