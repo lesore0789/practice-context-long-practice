238bd92eec1550807828bf9c2b8661d732d645c1
Leslie <lesore0789@gmail.com>
2023-02-02 14:51:07 -0800

    Finished problem set

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,21 @@
-// Your code here
+const Calculator = require("./02-calculator");
+
+class FancyCalculator extends Calculator {
+	setTotal = newTotal => this.total = newTotal;
+
+	modulo = num => this.total = this.total % num;
+
+	squared = () => this.total * this.total;
+}
+
+let fancyCalculator = new FancyCalculator();
+console.log(fancyCalculator.setTotal(5)); // => 5
+console.log(fancyCalculator.squared());   // => 25
+console.log(fancyCalculator.modulo(4));   // => 1
+console.log(fancyCalculator.total)        // => 1
+
+// can use instance methods on the Calculator class
+console.log(fancyCalculator.add(9))       // => 10
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
